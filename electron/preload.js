1b8dbcdf920e77b941fce7e777a180e3680d3041
지훈 김 <jhgim@idino.co.kr>
2023-06-24 01:47:27 +0900

    스타트 추가

--- electron/preload.js
+++ electron/preload.js
@@ -2,16 +2,9 @@ const { contextBridge, ipcMain, ipcRenderer } = require('electron')
 
 const readFileList = require('./helper/fileReader.js');
 //readFileList();
-
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector)
-      if (element) element.innerText = text
-    }
-    
-    for (const dependency of ['chrome', 'node', 'electron']) {
-      replaceText(`${dependency}-version`, process.versions[dependency])
-    }
-  })
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron
+  // we can also expose variables, not just functions
+})
